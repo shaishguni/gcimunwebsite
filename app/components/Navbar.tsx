f0f4fca97822c7b135686ada8e20eda8f629a86f
shaishguni <gunisguni2007@gmail.com>
2025-12-19 07:24:38 +0545

    navbar to be final

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,8 +1,20 @@
-import React from 'react'
+
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='flex space-x-6  items-center justify-between p-4 m-2'>
+      <div>GCI MUN</div>
+      <div>
+        <ul className='flex space-x-6 text-slate-500'>
+          <li>Home</li>
+          <li>Committees</li>
+          <li>Secretariat</li>
+          <li>Schedule</li>
+          <li>Sponsor</li>
+        </ul>
+      </div>
+      
+    </div>
   )
 }
 
