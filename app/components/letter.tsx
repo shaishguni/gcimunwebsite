2b768b907527ed1904bb64fff72769ba5cd3c6ea
shaishguni <gunisguni2007@gmail.com>
2026-01-03 13:26:36 +0545

    .

--- app/components/letter.tsx
+++ app/components/letter.tsx
@@ -5,19 +5,14 @@ const eb = EB_Garamond({
   weight: ["400"],
   variable: "--font-eb",
 });
-<<<<<<< HEAD
-const poppins = Poppins({
-    weight: ["400", "700"],
-    subsets: ["latin"],
-    variable: "--font-poppins",
-});
-=======
->>>>>>> f04eedab3a85d21aa13803df4154630a8e09dfa1
+
+
+
 
 export default function Letter() {
   return (
     <section
-      className={`${eb.variable} font-[var(--font-eb)] max-w-3xl mx-auto px-4 sm:px-6 md:px-12 py-8 sm:py-12 text-slate-600 text-sm sm:text-base leading-relaxed`}
+      className={`${eb.variable}  max-w-3xl mx-auto px-4 sm:px-6 md:px-12 py-8 sm:py-12 text-slate-600 text-sm sm:text-base leading-relaxed`}
     >
       <p className="mb-6">
         Distinguished Readers,
